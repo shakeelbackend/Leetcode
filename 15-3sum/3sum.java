00e4106fcbcbf455f9bbbfc14eb6921de905d5a5
Mohamed Shakeel S <shakeel.backenddev@gmail.com>
2026-03-04 02:16:09 +0530

    Time: 35 ms (44.08%) | Memory: 59.1 MB (75.11%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++) {
+        for(int i =0;i<n-2;i++)
+        {
+            if(i>0 && nums[i] ==nums[i-1]) continue;
 
+            int low = i+1;
+            int high = n-1;
 
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+            while(low< high)
+            {
+                int sum = nums[i]+nums[low]+nums[high];
 
-            int left = i + 1;
-            int right = nums.length - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0)
+                {
+                    result.add(Arrays.asList(nums[i],nums[low],nums[high]));
+                    while(low <high && nums[low] ==nums[low+1]) low++;
+                    while(low<high && nums[high] == nums[high-1]) high--;
 
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
+                    low++;
+                    high--;
                 }
-                else if (sum < 0) {
-                    left++;
+                else if(sum>0)
+                {
+                    high--;
                 }
-                else {
-                    right--;
+                else{
+                    low++;
                 }
             }
         }
-
         return result;
     }
 }
